6b8f93015c57d83c49a942cd76dc1d34aac155e4
collinhundley <collinhundley@gmail.com>
2016-09-03 16:21:49 -0600

    Linux fixes

--- Sources/Document.swift
+++ Sources/Document.swift
@@ -36,12 +36,12 @@ open class XMLDocument {
   
   /// The string encoding for the document. This is NSUTF8StringEncoding if no encoding is set, or it cannot be calculated.
   open fileprivate(set) lazy var encoding: String.Encoding = {
-    if let encodingName = ^-^self.cDocument.pointee.encoding {
-      let encoding = CFStringConvertIANACharSetNameToEncoding(encodingName as CFString!)
-      if encoding != kCFStringEncodingInvalidId {
-        return String.Encoding(rawValue: UInt(CFStringConvertEncodingToNSStringEncoding(encoding)))
-      }
-    }
+//    if let encodingName = ^-^self.cDocument.pointee.encoding {
+//      let encoding = CFStringConvertIANACharSetNameToEncoding(encodingName as CFString!)
+//      if encoding != kCFStringEncodingInvalidId {
+//        return String.Encoding(rawValue: UInt(CFStringConvertEncodingToNSStringEncoding(encoding)))
+//      }
+//    }
     return String.Encoding.utf8
   }()
 
