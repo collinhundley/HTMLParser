bfabc318c1310af4899c90b8e8b5622367bf0b8f
collinhundley <collinhundley@gmail.com>
2016-09-14 22:39:34 -0600

    Update C dependency

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "HTMLParser",
     dependencies: [
         // Module map for `libxml2`
-        .Package(url: "https://github.com/collinhundley/CLibXML2", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/collinhundley/CLibXML2", majorVersion: 1)
     ]
 )
